457e6f6708acf5cdb604cf8cf103dd6d798cde65
Matthew <mtwichan@gmail.com>
2024-11-09 21:56:15 -0500

    fix: missing useRef

--- components/ConvAI.tsx
+++ components/ConvAI.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useParams } from "next/navigation";
-import { useState, useEffect, useContext, createContext } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
